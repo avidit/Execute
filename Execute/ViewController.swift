017b1622d825911a5f9e2043920a631c2d1d8ea3
aabishkar <aabishkar_kc@cable.comcast.com>
2015-04-21 19:25:16 -0400

    Initial Commit

---
+++ Execute/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Execute
+//
+//  Created by aabishkar kc on 4/21/15.
+//  Copyright (c) 2015 aabishkar kc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
